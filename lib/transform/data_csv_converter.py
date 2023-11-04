54d7c8279f3d73592d001ecb0efc3524f013d531
Florian Schwanz <florian.schwanz@interoberlin.de>
2023-11-04 11:20:59 +0100

    chore: add csv converter (households by structure and usage type) #1

--- lib/transform/data_csv_converter.py
+++ lib/transform/data_csv_converter.py
@@ -36,6 +36,8 @@ def convert_data_to_csv(source_path, results_path, clean=False, quiet=False):
                 source_file_path, clean=clean, quiet=quiet)
             convert_file_to_csv_apartments_by_usage_type_building_size_heating_and_energy_type(
                 source_file_path, clean=clean, quiet=quiet)
+            convert_file_to_csv_households_by_structure_and_usage_type(
+                source_file_path, clean=clean, quiet=quiet)
 
 
 def convert_file_to_csv_apartments_by_size_year_of_construction_and_usage(
@@ -461,6 +463,49 @@ def convert_file_to_csv_apartments_by_usage_type_building_size_heating_and_energ
         print(f"✗️ Exception: {str(e)}")
 
 
+def convert_file_to_csv_households_by_structure_and_usage_type(source_file_path, clean=False, quiet=False):
+    source_file_name, source_file_extension = os.path.splitext(source_file_path)
+    file_path_csv = f"{source_file_name}-10-households-by-structure-and-usage-type.csv"
+
+    # Check if result needs to be generated
+    if not clean and os.path.exists(file_path_csv):
+        if not quiet:
+            print(f"✓ Already exists {os.path.basename(file_path_csv)}")
+        return
+
+    # Determine engine
+    engine = build_engine(source_file_extension)
+
+    try:
+        # Iterate over sheets
+        sheet = "Tab 10"
+        skiprows = 5
+        names = ["househould_structure", "households", "owners", "owners_percentage", "main_tenants",
+                 "main_tenants_percentage", "subtenants", "subtenants_percentage"]
+        drop_columns = []
+
+        dataframe = pd.read_excel(source_file_path, engine=engine, sheet_name=sheet, skiprows=skiprows, names=names,
+                                  index_col=False) \
+            .drop(columns=drop_columns, errors="ignore") \
+            .replace("–", 0) \
+            .replace("/", 0) \
+            .dropna() \
+            .assign(househould_structure=lambda df: df["househould_structure"].apply(lambda row: build_type_name(row)))
+
+        dataframe.reset_index(drop=True, inplace=True)
+        dataframe = dataframe.assign(type_index=lambda df: df.index) \
+            .assign(type_parent_index=lambda df: df.apply(lambda row: build_type_parent_index_10(row), axis=1)) \
+            .fillna(-1) \
+            .assign(type_parent_index=lambda df: df["type_parent_index"].astype(int))
+        dataframe.insert(0, "type_index", dataframe.pop("type_index"))
+        dataframe.insert(1, "type_parent_index", dataframe.pop("type_parent_index"))
+
+        # Write csv file
+        write_csv_file(dataframe, file_path_csv, quiet)
+    except Exception as e:
+        print(f"✗️ Exception: {str(e)}")
+
+
 #
 # Transformers
 #
@@ -539,7 +584,6 @@ def build_type_name(value):
     elif value == "21 und mehr Wohnungen":
         return "more_than_21_apartments"
 
-
     elif value == "Privatperson(en)":
         return "private_individuals"
     elif value == "privatwirtschaftliches Unternehmen":
@@ -548,6 +592,91 @@ def build_type_name(value):
         return "public_institution"
     elif value == "Wohnungs-/Baugenossenschaft":
         return "housing_building_cooperative"
+
+    elif value == "Haushalte":
+        return "households"
+    elif value == "mit 1 Person":
+        return "with_1_person"
+    elif value == "darunter weiblich":
+        return "female"
+    elif value == "mit 2 Personen":
+        return "with_2_persons"
+    elif value == "mit 3 Personen":
+        return "with_3_persons"
+    elif value == "mit 4 Personen":
+        return "with_4_persons"
+    elif value == "mit 5 und mehr Personen":
+        return "with_5_persons_or_more"
+    elif value == "mit Kindern unter 18 Jahren":
+        return "with_children_below_18"
+    elif value == "mit 1 Kind":
+        return "with_1_child"
+    elif value == "mit 2 Kindern":
+        return "with_2_children"
+    elif value == "mit 3 Kindern":
+        return "with_3_children"
+    elif value == "mit 4 und mehr Kindern":
+        return "with_4_children_or_more"
+    elif value == "ohne Kinder unter 18 Jahren":
+        return "without_children_below_18"
+    elif value == "unter 700":
+        return "net_household_income_below_700_euros"
+    elif value == "700  –     900":
+        return "net_household_income_between_700_and_900_euros"
+    elif value == "900  –  1 100":
+        return "net_household_income_between_900_and_1100_euros"
+    elif value == "1 100  –  1 300":
+        return "net_household_income_between_1100_and_1300_euros"
+    elif value == "1 300  –  1 500":
+        return "net_household_income_between_1300_and_1500_euros"
+    elif value == "1 500  –  2 000":
+        return "net_household_income_between_1500_and_2000_euros"
+    elif value == "2 000  –  3 200":
+        return "net_household_income_between_2000_and_3200_euros"
+    elif value == "3 200  –  4 500":
+        return "net_household_income_between_3200_and_4500_euros"
+    elif value == "4 500  –  6 000":
+        return "net_household_income_between_4500_and_6000_euros"
+    elif value == "6 000  –  7 500":
+        return "net_household_income_between_6000_and_7500_euros"
+    elif value == "7 500 und mehr":
+        return "net_household_income_above_7500_euros"
+    elif value == "1 Einkommensbezieher":
+        return "with_1_income_earner"
+    elif value == "2 und mehr Einkommensbezieher":
+        return "with_2_income_earner_or_more"
+    elif value == "Erwerbspersonen":
+        return "employable_persons"
+    elif value == "Erwerbstätige":
+        return "employed_persons"
+    elif value == "Selbstständige":
+        return "self_employed"
+    elif value == "Beamte":
+        return "civil_servants"
+    elif value == "Angestellte":
+        return "employed"
+    elif value == "Arbeiter":
+        return "workers"
+    elif value == "Erwerbslose":
+        return "unemployed_persons"
+    elif value == "Nichterwerbspersonen":
+        return "non_employable_persons"
+    elif value == "darunter Rentner":
+        return "pensioners"
+    elif value == "unter 25":
+        return "age_of_main_income_earner_below_25"
+    elif value == "25 – 30":
+        return "age_of_main_income_earner_between_25_and_30"
+    elif value == "30 – 40":
+        return "age_of_main_income_earner_between_30_and_40"
+    elif value == "40 – 50":
+        return "age_of_main_income_earner_between_40_and_50"
+    elif value == "50 – 60":
+        return "age_of_main_income_earner_between_50_and_60"
+    elif value == "60 – 65":
+        return "age_of_main_income_earner_between_60_and_65"
+    elif value == "65 und mehr":
+        return "age_of_main_income_earner_between_65_and_more"
 
     else:
         return value
@@ -1165,6 +1294,88 @@ def build_type_parent_index_10(row):
     row_index = row.name
 
     if row_index == 0:
+        return -1
+    elif row_index == 1:
+        return 0
+    elif row_index == 2:
+        return 1
+    elif row_index == 3:
+        return 0
+    elif row_index == 4:
+        return 0
+    elif row_index == 5:
+        return 0
+    elif row_index == 6:
+        return 0
+    elif row_index == 7:
+        return 0
+    elif row_index == 8:
+        return 7
+    elif row_index == 9:
+        return 7
+    elif row_index == 10:
+        return 7
+    elif row_index == 11:
+        return 7
+    elif row_index == 12:
+        return 0
+    elif row_index == 13:
+        return -1
+    elif row_index == 14:
+        return -1
+    elif row_index == 15:
+        return -1
+    elif row_index == 16:
+        return -1
+    elif row_index == 17:
+        return -1
+    elif row_index == 18:
+        return -1
+    elif row_index == 19:
+        return -1
+    elif row_index == 20:
+        return -1
+    elif row_index == 21:
+        return -1
+    elif row_index == 22:
+        return -1
+    elif row_index == 23:
+        return -1
+    elif row_index == 24:
+        return -1
+    elif row_index == 25:
+        return -1
+    elif row_index == 26:
+        return -1
+    elif row_index == 27:
+        return 26
+    elif row_index == 28:
+        return 27
+    elif row_index == 29:
+        return 27
+    elif row_index == 30:
+        return 27
+    elif row_index == 31:
+        return 27
+    elif row_index == 32:
+        return 26
+    elif row_index == 33:
+        return -1
+    elif row_index == 34:
+        return 33
+    elif row_index == 35:
+        return -1
+    elif row_index == 36:
+        return -1
+    elif row_index == 37:
+        return -1
+    elif row_index == 38:
+        return -1
+    elif row_index == 39:
+        return -1
+    elif row_index == 40:
+        return -1
+    elif row_index == 41:
         return -1
     else:
         return None
