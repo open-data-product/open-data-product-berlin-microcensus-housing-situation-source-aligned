c70e8d515e0abad0e9a5b0b75633450f1ad38117
Florian Schwanz <florian.schwanz@interoberlin.de>
2023-11-11 08:26:46 +0100

    chore: add csv converter (apartments in residential buildings by district and living area) #1

--- lib/transform/data_csv_converter.py
+++ lib/transform/data_csv_converter.py
@@ -50,6 +50,8 @@ def convert_data_to_csv(source_path, results_path, clean=False, quiet=False):
                 source_file_path, year, clean=clean, quiet=quiet)
             convert_file_to_csv_apartments_in_residential_buildings_by_district_usage_type_living_area_and_occupancy(
                 source_file_path, year, clean=clean, quiet=quiet)
+            convert_file_to_csv_apartments_in_residential_buildings_by_district_and_living_area(
+                source_file_path, year, clean=clean, quiet=quiet)
 
 
 def convert_file_to_csv_apartments_by_size_year_of_construction_and_usage(
@@ -707,6 +709,52 @@ def convert_file_to_csv_apartments_in_residential_buildings_by_district_usage_ty
         except Exception as e:
             print(f"✗️ Exception: {str(e)}")
 
+
+def convert_file_to_csv_apartments_in_residential_buildings_by_district_and_living_area(
+        source_file_path, year, clean=False, quiet=False):
+    tab_index = 22 if int(year) <= 2014 else 26
+    sheet = "Tab 22-23" if int(year) <= 2014 else "Tab 26-27"
+
+    source_file_name, source_file_extension = os.path.splitext(source_file_path)
+    file_path_csv = f"{source_file_name}-{tab_index}-apartments-in-residential-buildings-by-district-and-living-area.csv"
+
+    # Check if result needs to be generated
+    if not clean and os.path.exists(file_path_csv):
+        if not quiet:
+            print(f"✓ Already exists {os.path.basename(file_path_csv)}")
+        return
+
+    # Determine engine
+    engine = build_engine(source_file_extension)
+
+    try:
+        # Iterate over sheets
+        sheet = sheet
+        skiprows = 7
+        names = ["district_name", "apartments", "living_area_below_40sqm", "living_area_between_40_and_60sqm",
+                 "living_area_between_60_and_80sqm", "living_area_between_80_and_100sqm",
+                 "living_area_between_100_and_120sqm", "living_area_between_above_120sqm"]
+        drop_columns = []
+
+        dataframe = pd.read_excel(source_file_path, engine=engine, sheet_name=sheet, skiprows=skiprows, names=names,
+                                  index_col=False) \
+            .drop(columns=drop_columns, errors="ignore") \
+            .replace("/", 0) \
+            .assign(district_id=lambda df: df["district_name"].apply(lambda row: build_district_id(row))) \
+            .head(12) \
+            .drop("district_name", axis=1)
+
+        dataframe.reset_index(drop=True, inplace=True)
+        dataframe = dataframe.assign(
+            district_id=lambda df: df.apply(lambda row: str(row.name + 1).zfill(2), axis=1))
+
+        dataframe.reset_index(drop=True, inplace=True)
+        dataframe.insert(0, "district_id", dataframe.pop("district_id"))
+
+        # Write csv file
+        write_csv_file(dataframe, file_path_csv, quiet)
+    except Exception as e:
+        print(f"✗️ Exception: {str(e)}")
 
 #
 # Transformers
