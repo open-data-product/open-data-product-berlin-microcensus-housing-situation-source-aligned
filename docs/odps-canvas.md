9a157af86fbefdfbef3a74c6dd3f09b528219760
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-05-05 02:23:06 +0200

    chore: use open data product Python lib

---
+++ docs/odps-canvas.md
@@ -0,0 +1,28 @@
+
+# Open Data Product Specification (ODPS) Canvas - Berlin Microservice Housing Situation source-aligned
+
+* schema version: 3.1
+## Product
+
+### Basic Information
+
+* output file formats: csv
+* categories: timeseries
+* description: Data product providing Berlin microcensus housing situation data
+* logo URL: https://raw.githubusercontent.com/open-data-product/open-data-product-berlin-microcensus-housing-situation-source-aligned/refs/heads/main/logo-with-text.png
+* product ID: berlin-microcensus-housing-situation-source-aligned
+* product series: Berlin Timeseries Series
+* status: active
+* tags: Berlin, housing
+* type: source-aligned
+* value proposition: Providing Berlin microcensus housing situation data
+* version: 1.0
+* visibility: public
+
+### Data Holder
+
+* URL: https://opendataproduct.de
+
+
+---
+This Open Data Product Specification canvas is based on [OPEN DATA PRODUCT SPECIFICATION 3.1](https://opendataproducts.org/v3.1/#open-data-product-specification-3-1).
--- main.py
+++ main.py
@@ -1,64 +1,113 @@
-import getopt
 import os
 import sys
 
-from lib.extract.data_extractor import extract_data
-from lib.load.data_loader import load_data
-from lib.tracking_decorator import TrackingDecorator
-from lib.transform.data_copier import copy_data
-from lib.transform.data_csv_converter import convert_data_to_csv
+import click
+from opendataproduct.config.data_product_manifest_loader import (
+    load_data_product_manifest,
+)
+from opendataproduct.config.data_transformation_gold_loader import (
+    load_data_transformation_gold,
+)
+from opendataproduct.config.data_transformation_silver_loader import (
+    load_data_transformation_silver,
+)
+from opendataproduct.config.odps_loader import load_odps
+from opendataproduct.document.data_product_canvas_generator import (
+    generate_data_product_canvas,
+)
+from opendataproduct.document.data_product_manifest_updater import (
+    update_data_product_manifest,
+)
+from opendataproduct.document.odps_canvas_generator import generate_odps_canvas
+from opendataproduct.extract.data_extractor import extract_data
+from opendataproduct.transform.data_aggregator import aggregate_data
+from opendataproduct.transform.data_copier import copy_data
+from opendataproduct.transform.data_csv_converter import convert_data_to_csv
 
 file_path = os.path.realpath(__file__)
 script_path = os.path.dirname(file_path)
 
 
-@TrackingDecorator.track_time
-def main(argv):
-    # Set default values
-    clean = False
-    quiet = False
-
-    # Read command line arguments
-    try:
-        opts, args = getopt.getopt(argv, "hcq", ["help", "clean", "quiet"])
-    except getopt.GetoptError:
-        print("main.py --help --clean --quiet")
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            print("main.py")
-            print("--help                           show this help")
-            print("--clean                          clean intermediate results before start")
-            print("--quiet                          do not log outputs")
-            sys.exit()
-        elif opt in ("-c", "--clean"):
-            clean = True
-        elif opt in ("-q", "--quiet"):
-            quiet = True
-
-    manifest_path = os.path.join(script_path, "data-product.yml")
-    raw_path = os.path.join(script_path, "raw")
-    workspace_path = os.path.join(script_path, "workspace")
+@click.command()
+@click.option("--clean", default=False, help="Regenerate results.")
+@click.option("--quiet", default=False, help="Do not log outputs.")
+def main(clean, quiet):
     data_path = os.path.join(script_path, "data")
+    bronze_path = os.path.join(data_path, "01-bronze")
+    silver_path = os.path.join(data_path, "02-silver")
+    gold_path = os.path.join(data_path, "03-gold")
+    docs_path = os.path.join(script_path, "docs")
+
+    data_product_manifest = load_data_product_manifest(config_path=script_path)
+    data_transformation_silver = load_data_transformation_silver(
+        config_path=script_path
+    )
+    data_transformation_gold = load_data_transformation_gold(config_path=script_path)
+    odps = load_odps(config_path=script_path)
 
     #
-    # Extract
+    # Bronze: Integrate
     #
 
-    extract_data(manifest_path=manifest_path, results_path=raw_path, clean=clean, quiet=quiet)
+    extract_data(
+        data_product_manifest=data_product_manifest,
+        results_path=bronze_path,
+        clean=clean,
+        quiet=quiet,
+    )
 
     #
-    # Transform
+    # Silver: Transform
     #
 
-    copy_data(source_path=raw_path, results_path=workspace_path, clean=clean, quiet=quiet)
-    convert_data_to_csv(source_path=workspace_path, results_path=workspace_path, clean=clean, quiet=quiet)
+    copy_data(
+        data_transformation=data_transformation_silver,
+        source_path=bronze_path,
+        results_path=silver_path,
+        clean=clean,
+        quiet=quiet,
+    )
+
+    convert_data_to_csv(
+        data_transformation=data_transformation_silver,
+        source_path=silver_path,
+        results_path=silver_path,
+        clean=clean,
+        quiet=quiet,
+    )
 
     #
-    # Load
+    # Gold: Aggregate
     #
 
-    load_data(source_path=workspace_path, results_path=data_path, clean=clean, quiet=quiet)
+    aggregate_data(
+        data_transformation=data_transformation_gold,
+        source_path=silver_path,
+        results_path=gold_path,
+        clean=clean,
+        quiet=quiet,
+    )
+
+    #
+    # Documentation
+    #
+
+    update_data_product_manifest(
+        data_product_manifest=data_product_manifest,
+        config_path=script_path,
+        data_paths=[silver_path, gold_path],
+        file_endings=(".csv"),
+    )
+
+    generate_data_product_canvas(
+        data_product_manifest=data_product_manifest,
+        docs_path=docs_path,
+    )
+
+    generate_odps_canvas(
+        odps=odps,
+        docs_path=docs_path,
+    )
 
 
 if __name__ == "__main__":
